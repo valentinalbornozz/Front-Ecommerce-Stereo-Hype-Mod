fc0fc717e7c816321473749f3a7a0fb255465d89
Valentin Albornoz <valeg486@gmail.com>
2024-05-13 16:17:20 -0300

    ajuste sección de productos
    ajustamos el diseño de la parte de la sección de productos, ya se le puso el nombre con fondo amarrillo, se arreglo el orden de los productos y se agrego diseño al boton de add to cart

--- src/components/Home/Products/product.jsx
+++ src/components/Home/Products/product.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import products from "./Products";
 import "./Products.css";
-// Agrega un estado inicial para manejar las cantidades de cada producto
+
 const initialQuantities = products.reduce((acc, product) => {
   acc[product.id] = 1;
   return acc;
@@ -30,40 +30,68 @@ const Producto = () => {
       <div className="product-container">
         {products.map((product, index) => (
           <div key={index} className="product-card">
-            <img
-              src={product.imageUrl}
-              alt={product.name}
-              className="product-image"
-            />
+            <div className="product-image-container">
+              <img
+                src={product.imageUrl}
+                alt={product.name}
+                className="product-image"
+              />
+            </div>
             <div className="product-info">
-              <h3 className="product-name">{product.name}</h3>
-              <p className="product-price">{product.price}</p>
-              <p>Size</p>
-              {product.sizes && product.sizes.length > 0 && (
-                <select className="product-sizes">
-                  {product.sizes.map((size) => (
-                    <option key={size}>{size}</option>
-                  ))}
-                </select>
-              )}
-              <p>quantity</p>
+              <div className="product-details">
+                <div className="product-title">
+                  <a href="#" className="product-name">
+                    {product.name}
+                  </a>
+                </div>
 
-              <div className="quantity-controls">
-                <button
-                  className="decrement-button"
-                  onClick={() => decrementQuantity(product.id)}
-                >
-                  -
-                </button>
-                <span className="quantity">{quantities[product.id]}</span>
-                <button
-                  className="increment-button"
-                  onClick={() => incrementQuantity(product.id)}
-                >
-                  +
-                </button>
+                <p className="product-price">{product.price}</p>
+                <p>
+                  <a href="#" className="shipping-link">
+                    Shipping
+                  </a>{" "}
+                  calculated at checkout.
+                </p>
+                <div className="size-quantity-container">
+                  {product.sizes && product.sizes.length > 0 && (
+                    <div className="size-selector">
+                      <label htmlFor={`size-select-${product.id}`}>Size</label>
+                      <select
+                        id={`size-select-${product.id}`}
+                        className="product-sizes"
+                      >
+                        {product.sizes.map((size) => (
+                          <option key={size}>{size}</option>
+                        ))}
+                      </select>
+                    </div>
+                  )}
+                  <div className="quantity-selector">
+                    <label htmlFor={`quantity-select-${product.id}`}>
+                      Quantity
+                    </label>
+                    <div className="quantity-controls">
+                      <button
+                        className="decrement-button"
+                        onClick={() => decrementQuantity(product.id)}
+                      >
+                        -
+                      </button>
+                      <span className="quantity">{quantities[product.id]}</span>
+                      <button
+                        className="increment-button"
+                        onClick={() => incrementQuantity(product.id)}
+                      >
+                        +
+                      </button>
+                    </div>
+                  </div>
+                  <button className="add-to-cart-button">ADD TO CART</button>
+                </div>
               </div>
-              <button className="add-to-cart-button">Agregar al carrito</button>
+              <a href="#" className="full-details-link">
+                Full Details
+              </a>
             </div>
           </div>
         ))}
