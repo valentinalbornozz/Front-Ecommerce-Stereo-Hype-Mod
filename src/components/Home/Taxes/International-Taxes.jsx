7f22da828f4bad57417efafbaf71bb51b07827d9
Valentin Albornoz <valeg486@gmail.com>
2024-05-14 12:59:39 -0300

    agregado de international taxes

---
+++ src/components/Home/Taxes/International-Taxes.jsx
@@ -0,0 +1,23 @@
+const InternationalTaxes = () => {
+  return (
+    <div
+      className="text-center flex  items-center justify-center"
+      style={{ padding: "30px" }}
+    >
+      <div className="bg-white p-7">
+        <h1 className=" font-bold text-black mb-4" style={{ fontSize: "30px" }}>
+          INTERNATIONAL TAXES
+        </h1>
+        <p className="font-size-14 text-black mb-2">
+          Please be aware that your package may be subject to additional local
+          taxes.
+        </p>
+        <p className="font-size-14 text-black">
+          This is completely out of our control.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default InternationalTaxes;
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,11 +1,13 @@
 import Hero from "../components/Home/Hero/Hero";
 import Producto from "../components/Home/Products/product";
+import Taxes from "../components/Home/Taxes/International-Taxes";
 
 const Home = () => {
   return (
     <>
       <Hero />
       <Producto />
+      <Taxes />
     </>
   );
 };
